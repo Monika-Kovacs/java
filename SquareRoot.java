94a41128af76bcd728e0beab20c7e18d578ab323
Monika-Kovacs <71174212+Monika-Kovacs@users.noreply.github.com>
2021-03-17 11:58:26 +0000

    Add files via upload

---
+++ SquareRoot.java
@@ -0,0 +1,30 @@
+/* find square root of a through guessing and incrementally getting closer. delta < 0.0001
+ * Initial guess = a/2
+ * x1 = (x0 + a/x0) / 2
+ * 
+ */
+public class SquareRoot {
+	
+	public static double sqrt(double a) {
+		
+		double delta = 0.0001;
+		double guess = a / 2;
+		double improvedGuess = (guess + a/guess) / 2;
+		
+		while (Math.abs(improvedGuess - guess) > delta) {
+			
+			guess = improvedGuess;
+			improvedGuess = (guess + a / guess) / 2;
+		}
+		
+		return improvedGuess;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(sqrt(25));
+	
+	
+		}
+
+}
