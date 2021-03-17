629f78bdf32089105a63afd66daccda14ac37bdc
Monika-Kovacs <71174212+Monika-Kovacs@users.noreply.github.com>
2021-03-17 17:36:26 +0000

    Update CylinderArea.java

--- CylinderArea.java
+++ CylinderArea.java
@@ -4,12 +4,12 @@
 
 
 public class CylinderArea {
-	
+	// method for cylinder area calculation in general
 	public static double cylinderCalculation(double radius, double height) {
 		
 		return 2 * Math.PI * radius * height + 2 * Math.PI * (radius * radius);
 	}
-	
+	// radius = diameter / 2
 	public static void main (String[] args) {
 		
 		double diameter = 10.0;
